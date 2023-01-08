539f149f744b3c0a26f92f5ff9bc6a7550d42adb
maxim1770 <maxim1770@gmail.com>
2023-01-08 23:34:56 +0300

    refactor(api/tags): в routers tags заменить на с str на Enum
    Из docs fastapi: Tags with Enums - https://fastapi.tiangolo.com/tutorial/path-operation-configuration/?h=enum+tags#tags-with-enums

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -7,41 +7,39 @@ from app.api.api_v1.endpoints.holiday import api as holiday
 from app.api.api_v1.endpoints.movable_date import api as movable_date
 from app.api.api_v1.endpoints.saint import api as saint
 from app.db.session import engine, Base
+from enum import StrEnum, auto
 
 Base.metadata.create_all(bind=engine)
 
+
+class RouterTag(StrEnum):
+    bible_books = auto()
+    zachalos = auto()
+
+    books = auto()
+
+    cycles = auto()
+    weeks = auto()
+    days = auto()
+    movable_dates = auto()
+
+    readings = auto()
+
+    saints = auto()
+
+    holidays = auto()
+
+
 api_router = APIRouter()
 
-api_router.include_router(bible_book.router, prefix="/bible_books", tags=["bible-books"])
+api_router.include_router(bible_book.router, prefix="/bible-books", tags=[RouterTag.bible_books])
 
-api_router.include_router(book.router, prefix="/books", tags=["books"])
+api_router.include_router(movable_date.router, prefix="/movable-dates", tags=[RouterTag.movable_dates])
 
-api_router.include_router(movable_date.router, prefix="/movable-dates", tags=["movable-dates"])
+api_router.include_router(book.router, prefix="/books", tags=[RouterTag.books])
 
-api_router.include_router(saint.router, prefix="/saints", tags=["saints"])
+api_router.include_router(reading.router, prefix="/readings", tags=[RouterTag.readings])
 
-api_router.include_router(holiday.router, prefix="/holidays", tags=["holidays"])
+api_router.include_router(saint.router, prefix="/saints", tags=[RouterTag.saints])
 
-
-api_router.include_router(reading.router, prefix="/readings", tags=["readings"])
-
-# router = APIRouter(
-#     prefix="/bible_books",
-#     tags=[Tags.bible_books],
-#     responses={404: {"description": "Not found"}},
-# )
-
-
-# router = APIRouter(
-#     prefix='/cycles',
-#     tags=[Tags.cycles],
-#     responses={404: {"description": "Not found"}},
-# )
-
-# class Tags(str, Enum):
-#     bible_books = 'bible_books'
-#     zachalos = 'zachalos'
-#     cycles = 'cycles'
-#     users = 'users'
-#     weeks = 'weeks'
-#     days = 'days'
+api_router.include_router(holiday.router, prefix="/holidays", tags=[RouterTag.holidays])
