7538ed8d10d86e4b186ec5d6d2819dfea8b94ada
maxim1770 <maxim1770@gmail.com>
2023-01-26 14:02:38 +0300

    test(create/prepare): добавить тесты для тестирования общей работы всего класса SaintHolidayCreateFactory
    Который на выходе должен давать готовую схему SaintHolidayCreate
    из который уже спокойно можно создавать запись в бд через create_saint_holiday()

--- app/tests/create/prepare/holiday/test_saint_holiday_create_factory.py
+++ app/tests/create/prepare/holiday/test_saint_holiday_create_factory.py
@@ -2,7 +2,113 @@ from datetime import date
 
 import pytest
 
+from app import schemas, enums
 from app.create.prepare.holiday.saint_holiday import SaintHolidayCollect, SaintHolidayCreateFactory
+
+some_day: date = date(2023, 3, 25)
+some_saint_slug: str = 'foo-bar'
+
+
+@pytest.mark.parametrize('saint_holiday_collect, saint_holiday_in, _year', [
+    (
+            SaintHolidayCollect(
+                day=some_day,
+                saint_slug=some_saint_slug,
+                full_title='text (1234);'
+            ),
+            schemas.SaintHolidayCreate(
+                holiday_in=schemas.HolidayCreate(
+                    slug=f'den-pamjati-{some_saint_slug}',
+                    title='Text'
+                ),
+                holiday_category_title=enums.HolidayCategoryTitle.den_pamjati,
+                saint_in=schemas.SaintCreate(slug=some_saint_slug),
+                year_in=schemas.YearCreate(title='1242'),
+                day_in=schemas.DayCreate(month=some_day.month, day=some_day.day)
+            ),
+            5500 + 8 + 1234
+    ),
+    (
+            SaintHolidayCollect(
+                day=some_day,
+                saint_slug=some_saint_slug.replace('-', ''),
+                full_title='Перенесение мощей text (XVI);'
+            ),
+            schemas.SaintHolidayCreate(
+                holiday_in=schemas.HolidayCreate(
+                    slug=f'perenesenie-moschej-{some_saint_slug.replace("-", "")}',
+                    title='Перенесение мощей text'
+                ),
+                holiday_category_title=enums.HolidayCategoryTitle.perenesenie_moschej,
+                saint_in=schemas.SaintCreate(slug=some_saint_slug.replace('-', '')),
+                year_in=schemas.YearCreate(title=f'XVI'),
+                day_in=schemas.DayCreate(month=some_day.month, day=some_day.day)
+            ),
+            5500 + 8 + 1550
+    ),
+    (
+            SaintHolidayCollect(
+                day=some_day,
+                saint_slug=some_saint_slug,
+                full_title='Обре́тение мощей Text (ок.1556- 1557)'
+            ),
+            schemas.SaintHolidayCreate(
+                holiday_in=schemas.HolidayCreate(
+                    slug=f'obretenie-moschej-{some_saint_slug}',
+                    title='Обре́тение мощей Text'
+                ),
+                holiday_category_title=enums.HolidayCategoryTitle.obretenie_moschej,
+                saint_in=schemas.SaintCreate(slug=some_saint_slug),
+                year_in=schemas.YearCreate(title='ок. 1564-1565'),
+                day_in=schemas.DayCreate(month=some_day.month, day=some_day.day)
+            ),
+            5500 + 8 + 1557
+    ),
+    (
+            SaintHolidayCollect(
+                day=some_day,
+                saint_slug=some_saint_slug,
+                full_title='обре́тение мощей Text (после XV) (Болг.);'
+            ),
+            schemas.SaintHolidayCreate(
+                holiday_in=schemas.HolidayCreate(
+                    slug=f'obretenie-moschej-{some_saint_slug}',
+                    title='Обре́тение мощей Text'
+                ),
+                holiday_category_title=enums.HolidayCategoryTitle.obretenie_moschej,
+                saint_in=schemas.SaintCreate(slug=some_saint_slug),
+                year_in=schemas.YearCreate(title='после XV'),
+                day_in=schemas.DayCreate(month=some_day.month, day=some_day.day)
+            ),
+            5500 + 8 + 1500
+    ),
+    (
+            SaintHolidayCollect(
+                day=some_day,
+                saint_slug=some_saint_slug,
+                full_title='Преставление text (407);'
+            ),
+            schemas.SaintHolidayCreate(
+                holiday_in=schemas.HolidayCreate(
+                    slug=f'den-pamjati-{some_saint_slug}',
+                    title='Преставление text'
+                ),
+                holiday_category_title=enums.HolidayCategoryTitle.den_pamjati,
+                saint_in=schemas.SaintCreate(slug=some_saint_slug),
+                year_in=schemas.YearCreate(title='415'),
+                day_in=schemas.DayCreate(month=some_day.month, day=some_day.day)
+            ),
+            5500 + 8 + 407
+    ),
+])
+def test_saint_holiday_create_factory(
+        saint_holiday_collect: SaintHolidayCollect,
+        saint_holiday_in: schemas.SaintHolidayCreate,
+        _year: int
+) -> None:
+    created_saint_holiday_in = SaintHolidayCreateFactory(saint_holiday_collect).get()
+    assert created_saint_holiday_in == saint_holiday_in
+    assert created_saint_holiday_in.year_in.year == _year
 
 
 @pytest.mark.parametrize('full_title, year_title', [
@@ -16,7 +122,7 @@ from app.create.prepare.holiday.saint_holiday import SaintHolidayCollect, SaintH
 ])
 def test_clean_year_title(full_title: str, year_title: str) -> None:
     assert SaintHolidayCreateFactory(
-        SaintHolidayCollect(day=date(2023, 3, 25), saint_slug='foo', full_title=full_title)
+        SaintHolidayCollect(day=some_day, saint_slug=some_saint_slug, full_title=full_title)
     )._clean_year_title() == year_title
 
 
@@ -29,7 +135,7 @@ def test_clean_year_title(full_title: str, year_title: str) -> None:
 ])
 def test_offset_years_in_year_title(full_title: str, year_title: str) -> None:
     assert SaintHolidayCreateFactory(
-        SaintHolidayCollect(day=date(2023, 3, 25), saint_slug='foo', full_title=full_title)
+        SaintHolidayCollect(day=some_day, saint_slug=some_saint_slug, full_title=full_title)
     )._offset_years_in_year_title() == year_title
 
 
@@ -55,5 +161,5 @@ def test_offset_years_in_year_title(full_title: str, year_title: str) -> None:
 ])
 def test_clean_holiday_title(full_title: str, holiday_title: str) -> None:
     assert SaintHolidayCreateFactory(
-        SaintHolidayCollect(day=date(2023, 3, 25), saint_slug='foo', full_title=full_title)
+        SaintHolidayCollect(day=some_day, saint_slug=some_saint_slug, full_title=full_title)
     )._clean_holiday_title() == holiday_title
