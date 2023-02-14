ea11d7968f65bd4030a1aac499d5c247b711a56f
maxim1770 <maxim1770@gmail.com>
2023-02-14 23:09:28 +0300

    fix(db): убрать все изменения, как было в master
    Потому что __table_args__ = {'extend_existing': True}
    это вообще не нужно, и это костыли какие то
    А POSTGRES_INDEXES_NAMING_CONVENTION возможно потом добавится

--- app/db/base_class.py
+++ app/db/base_class.py
@@ -11,16 +11,6 @@ REGEX_CAMEL_TO_SNAKE: Pattern[str] = re.compile(r'(?<!^)(?=[A-Z])')
 intpk = Annotated[int, mapped_column(primary_key=True)]
 unique_slug = Annotated[str, mapped_column(String(150), unique=True)]
 
-# POSTGRES_INDEXES_NAMING_CONVENTION = {
-#     "ix": "%(column_0_label)s_idx",
-#     "uq": "%(table_name)s_%(column_0_name)s_key",
-#     "ck": "%(table_name)s_%(constraint_name)s_check",
-#     "fk": "%(table_name)s_%(column_0_name)s_fkey",
-#     "pk": "%(table_name)s_pkey",
-# }
-
-# metadata_obj = MetaData(naming_convention=POSTGRES_INDEXES_NAMING_CONVENTION)
-
 metadata_obj = MetaData()
 
 
@@ -31,5 +21,3 @@ class Base(DeclarativeBase):
     @classmethod
     def __tablename__(cls) -> str:
         return REGEX_CAMEL_TO_SNAKE.sub('_', cls.__name__).lower()
-
-    __table_args__ = {'extend_existing': True}
