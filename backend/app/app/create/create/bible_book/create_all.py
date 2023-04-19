56c3c9aa4e870ebd8d3e7adebfa3581da96fb34a
maxim1770 <maxim1770@gmail.com>
2023-04-19 22:20:12 +0300

    fix(zachalos):

--- backend/app/app/create/create/bible_book/create_all.py
+++ backend/app/app/create/create/bible_book/create_all.py
@@ -1,7 +1,9 @@
 import logging
 
+import sqlalchemy as sa
 from sqlalchemy.orm import Session
 
+from app import models, enums
 from .bible_book import create_bible_books
 from .zachalos_movable_dates import CreateZachaloMovableDateAssociationFactory
 
@@ -21,3 +23,13 @@ def create_c2_zachalos_movable_dates_associations(db: Session):
     create_zachalo_movable_date_association = CreateZachaloMovableDateAssociationFactory.get_c2(db)
     zachalos_id: list[int] = create_zachalo_movable_date_association.create()
     logging.info('Created c2 zachalos movable dates associations')
+    zachalos = db.execute(sa.select(models.Zachalo).join(models.BibleBook).filter(
+        models.BibleBook.abbr == enums.BibleBookAbbr.Apok)).scalars().all()
+    for zachalo in zachalos:
+        zachalos_movable_dates = db.execute(
+            sa.select(models.ZachalosMovableDates).filter_by(zachalo_id=zachalo.id)
+        ).scalar_one_or_none()
+        db.delete(zachalos_movable_dates)
+        db.delete(zachalo)
+        db.commit()
+    logging.info('Deleted default (BibleBookAbbr = Apok) c2 zachalos and zachalos movable dates associations')
