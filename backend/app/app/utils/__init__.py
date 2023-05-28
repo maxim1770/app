4574ea5a9a6e2d03855d7155556baf91da01dda5
maxim1770 <maxim1770@gmail.com>
2023-05-28 10:48:08 +0300

    to join 2

--- backend/app/app/utils/__init__.py
+++ backend/app/app/utils/__init__.py
@@ -1,5 +1,4 @@
-from .common import enum2regex, clean_extra_spaces
-from .manuscript import PrepareManuscriptPath
+from .common import enum2regex
 from .manuscript import is_rsl_manuscript_code, is_rsl_manuscript_code_title, is_rsl_library, prepare_manuscript_url, \
     prepare_manuscript_neb_url
 from .manuscript import pages_in2pages_nums
